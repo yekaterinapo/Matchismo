3697578134ff53430f90cc41e93c0b9a5aeec6a2
Yekaterina Podiatchev <yekaterina@ligtricks.com>
2019-09-19 14:50:50 +0300

    - fix problem with import (copied reference to file)

---
+++ Matchismo/views/SetCardView.h
@@ -0,0 +1,17 @@
+//
+//  SetCardView.h
+//  Matchismo
+//
+//  Created by Yekaterina Podiatchev on 19/09/2019.
+//  Copyright © 2019 Yekaterina Podiatchev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SetCardView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Matchismo/views/SetCardView.m
@@ -0,0 +1,21 @@
+//
+//  SetCardView.m
+//  Matchismo
+//
+//  Created by Yekaterina Podiatchev on 19/09/2019.
+//  Copyright © 2019 Yekaterina Podiatchev. All rights reserved.
+//
+
+#import "SetCardView.h"
+
+@implementation SetCardView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
